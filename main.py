1bd9a66597ba009710d9c01e5795932844fb5851
TarikSalles <tarik.paiva@ufv.br>
2024-12-05 18:35:44 -0300

    fix: correção teclas

--- main.py
+++ main.py
@@ -5,55 +5,76 @@ import random
 from music import Music
 
 def main():
-    screen_width = 800
+    screen_width = 1000
     screen_height = 600
     tamanho_linha_width = 700
     tamanho_linha_height = 100
-    Score_Geral = 0
+    music_player1 =  Music("Info.dat", "ExpertPlusStandard.dat", "miser.mp3")
+    music_player2 =  Music("Info.dat", "ExpertPlusStandard.dat", "miser.mp3")
 
-    controlador = ControladorEsferas()
+    score_player1 = 0
+    score_player2 = 0
+    tempoAtual = 0.0
+    tempoDuracao = 15.0
+    turnoPlayer1 = True
+    controlador_esferas_player_1 = ControladorEsferas()
+    controlador_esferas_player_2 = ControladorEsferas()
 
-    music =  Music("Info.dat", "ExpertPlusStandard.dat", "miser.mp3")
-    piano = Piano(controlador, width=screen_width, height=screen_height / 2, 
-                  
-                  tamanho_linha_width=tamanho_linha_width, tamanho_linha_height=tamanho_linha_height, score = Score_Geral, tipo = "ataque",music=music)
 
-    piano.run_music()
+    beats_player1 = Piano(controlador_esferas_player_1, width=screen_width / 2, height=screen_height / 2, tamanho_linha_width=tamanho_linha_width, tamanho_linha_height=tamanho_linha_height, score = score_player1, tipo = "ataque",music=music_player1,teclas = [pygame.K_w, pygame.K_a, pygame.K_s, pygame.K_d])
+    beats_player2 = Piano(controlador_esferas_player_2, width=screen_width / 2, height=screen_height / 2, tamanho_linha_width=tamanho_linha_width, tamanho_linha_height=tamanho_linha_height, score = score_player2, tipo = "ataque",music=music_player2,init_x= screen_width / 2 + 50, init_y=0,teclas = [pygame.K_UP, pygame.K_LEFT, pygame.K_DOWN, pygame.K_RIGHT])
+
+    beats_player1.run_music()
+    beats_player2.run_music()
+
     screen = pygame.display.set_mode((screen_width, screen_height))
-    pygame.display.set_caption("Piano Interativo")
-
-    
-
+    pygame.display.set_caption("")
     running = True
     clock = pygame.time.Clock()
 
     while running:
+        screen.fill((255, 255, 255))
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LSHIFT:
-                    print("Score Geral: ", piano.get_score_geral())
-                    letra_aleatoria = ["W", "A", "S", "D"]
-                    piano.criar_esfera(random.choice(letra_aleatoria))
-
-        keys_pressed = pygame.key.get_pressed()
-        #piano.verificar_colisao(keys_pressed)
             
+        
 
+        for esfera in controlador_esferas_player_1.esferas:
+                esfera.mover() 
+                if esfera.update():
+                    controlador_esferas_player_1.remover_esfera(esfera)
+        
 
-        for esfera in controlador.esferas:
-            esfera.mover() 
-            if esfera.update():
-                controlador.remover_esfera(esfera)
-            
+      
+        for esfera in controlador_esferas_player_2.esferas:
+                esfera.mover() 
+                if esfera.update():
+                    controlador_esferas_player_2.remover_esfera(esfera)
+        
+        #beats_player1.piano_loop_principal()
+        #beats_player2.piano_loop_principal()
+    
+        #beats_player1.spawn_notes()
+        beats_player2.spawn_notes()
 
+        beats_player1.spawn_notes()
+        beats_player1.update_notes()
+        beats_player2.update_notes()
+
+        beats_player1.verificar_colisao()  # Verifique colisões para todas as teclas pressionadas
 
         
-        piano.desenhar(screen)
-        piano.piano_loop_principal()
+        beats_player2.verificar_colisao()  # Verifique colisões para todas as teclas pressionadas
+
+        beats_player1.desenhar(screen)
+        beats_player2.desenhar(screen)
+    
+
         pygame.display.flip()
         clock.tick(60)
+        
 
     pygame.quit()
 
